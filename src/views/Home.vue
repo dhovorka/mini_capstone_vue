09d933803077c19f1a52f645b591368719ac5eae
Daniel Hovorka <dhovorka10@gmail.com>
2019-03-31 13:03:09 -0500

    initial commit

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,46 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <button v-on:click="createProduct()">Create product</button>
+    <h1>{{ message }}</h1>
+    <p>{{ name }}</p>
+    <div v-for="product in products">
+      <h2>Title: {{ product.name }}</h2>
+      <img v-bind:src="product.images" v-bind:alt="product.title" />
+    </div>
   </div>
 </template>
 
+<style></style>
+
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+var axios = require("axios");
 
 export default {
-  name: 'home',
-  components: {
-    HelloWorld
+  data: function() {
+    return {
+      message: "Welcome to Vue.js!",
+      name: "Dan",
+      products: []
+    };
+  },
+  created: function() {
+    axios.get("/api/products").then(response => {
+      this.products = response.data;
+    });
+  },
+  methods: {
+    createProduct: function() {
+      console.log("create a product...");
+      var params = {
+        name: "test name",
+        price: "test price",
+        description: "test description",
+        supplier_id: 1
+      };
+      axios.post("/api/products", params).then(response => {
+        console.log("success", response.data);
+      });
+    }
   }
-}
+};
 </script>
